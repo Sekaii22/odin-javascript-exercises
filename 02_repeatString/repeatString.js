f2409face5a95dce689fda38066f5e0db4bb5a88
Sekaii22 <shawn.ng22@gmail.com>
2025-03-26 04:03:07 +0800

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    let result = "";
 
+    if (count < 0) result = "ERROR";
+
+    for (let i = 0; i < count; i++) {
+        result = result + str;
+    }
+    return result;
 };
 
 // Do not edit below this line
