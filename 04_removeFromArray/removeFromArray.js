bb681cdbe7a8ee8cef21ff20e6087e02e7ee93c0
Sekaii22 <shawn.ng22@gmail.com>
2025-03-26 04:30:11 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    // new copy
+    newArr = arr.slice();
+
+    // for each number to remove
+    for (let num of nums) {
+        let index;
+
+        // keep removing until that number has been fully removed
+        while((index = newArr.findIndex(item => item === num)) !== -1) {
+            newArr.splice(index, 1);
+        }
+    }
+
+    return newArr
 };
 
 // Do not edit below this line
