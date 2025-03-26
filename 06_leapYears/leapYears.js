82c842011b155d4998ab9c99ae7c69291d7d16cc
Sekaii22 <shawn.ng22@gmail.com>
2025-03-26 22:42:35 +0800

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // leap year must be divisible by 4
+    if (year % 4 == 0) {
+        // leap year not divisible by 100 is guaranteed to be leap year
+        if (year % 100 == 0) {
+            // if divisible by 100, if it is also divisible by 400, 
+            // then guaranteed to be leap year
+            if (year % 400 == 0) return true;
+            else return false
+        }
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
