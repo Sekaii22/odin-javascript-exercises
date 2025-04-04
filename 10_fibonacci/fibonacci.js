bc47c192172268fb45db5b5d54cd5196da4d84ec
Sekaii22 <shawn.ng22@gmail.com>
2025-04-05 00:44:32 +0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
 
+    if (+num === 0) return 0;
+    else if (+num < 0) return "OOPS";
+
+    for (let i = 2; i <= +num; i++) {
+        // xn = xn−1 + xn−2
+        [a, b] = [b, a+b];
+    }
+    return b;
 };
 
 // Do not edit below this line
