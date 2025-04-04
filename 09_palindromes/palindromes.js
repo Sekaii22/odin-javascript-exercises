147562005e4028b76bb091039e31e119d8eb8bd1
Sekaii22 <shawn.ng22@gmail.com>
2025-04-05 00:30:52 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strClean = str.toLowerCase().split("")
+                        .filter(char => {
+                            let code = char.charCodeAt(0);
+                            return (code >= 48 && code <= 57) || 
+                            (code >= 65 && code <= 90) || 
+                            (code >= 97 && code <= 122);
+                        })
+                        .join("");
 
+    let strReverse = strClean.split("")
+                        .reverse()
+                        .join("");
+
+    console.log(strReverse);
+
+    return strClean === strReverse;
 };
 
 // Do not edit below this line
