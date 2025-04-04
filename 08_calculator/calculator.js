8dd8516517278d3388c7c1aebe8c6c1ba4a21d71
Sekaii22 <shawn.ng22@gmail.com>
2025-04-05 00:07:01 +0800

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return +a - +b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ return arr.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // n! = n * (n-1)!
+	let a = 1;
+
+  for (let i = 2; i <= num; i++) {
+    a = i * a;
+  }
+  return a;
 };
 
 // Do not edit below this line
