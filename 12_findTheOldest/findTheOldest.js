96817bfdb46259b3e900edd7c18c48af60a7d5fb
Sekaii22 <shawn.ng22@gmail.com>
2025-04-05 01:07:34 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+        let currentYear = new Date().getFullYear();
 
+        let personAge = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth;
+        
+        return oldestAge >= personAge ? oldest : person;
+    });
 };
 
 // Do not edit below this line
